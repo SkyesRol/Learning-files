4a4fa786761e72c7bae48eadcaef0fc46984fe90
SkyesRol <1771821463@qq.com>
2025-07-07 12:14:54 +0800

    盒子模型/flex/z-index
    学习了两种不同的盒子模型，又复习了一遍flex布局，详细讲解了z-index和文档流

---
+++ js/js_let-const/3.js
@@ -0,0 +1,38 @@
+console.log(name);
+
+
+var name = 'Skye';
+
+
+function add(a,b){
+    return a,b;
+}
+
+{
+    const pos = 'Initiator';
+    let weapon = 'vandal';
+    console.log(weapon);
+    
+}
+
+console.log(pos);
+
+add(1,3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/js_let-const/4.js
@@ -0,0 +1,9 @@
+console.log(a);
+var a = 3;
+let c ;
+function add(a,b){
+    console.log(a,b);
+    console.log(c);
+    
+}
+add(1,3);
